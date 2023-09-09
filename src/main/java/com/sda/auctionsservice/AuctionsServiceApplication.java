81f411bdd39c729917736bb0d10bfe2e628a8425
Mac Mini <macmini@Macs-Mac-mini.local>
2023-09-09 11:50:24 +0200

    first commit

---
+++ src/main/java/com/sda/auctionsservice/AuctionsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sda.auctionsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sda/auctionsservice/AuctionsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.auctionsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
